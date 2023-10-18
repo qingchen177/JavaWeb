f840ad72d65596e3d9713a2ca399ceeb917d24af
ljl <ljl@chinaliancheng.com>
2023-10-18 11:20:43 +0800

    location: company message: AOP切面

--- basic-web/src/main/java/top/qingchen/basicweb/entity/AuditRecord.java
+++ basic-web/src/main/java/top/qingchen/basicweb/entity/AuditRecord.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -15,8 +17,14 @@ import java.time.LocalDateTime;
  * </p>
  *
  * @author qingchen
- * @since 2023-10-17
+ * @since 2023-10-18
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode()
+@Accessors(chain = true)
 @TableName("qingchen_basic_web_audit_record")
 @ApiModel(value = "AuditRecord对象", description = "审计记录表")
 public class AuditRecord implements Serializable {
@@ -29,17 +37,26 @@ public class AuditRecord implements Serializable {
     @ApiModelProperty("操作人")
     private String operator;
 
-    @ApiModelProperty("操作")
-    private String operation;
+    @ApiModelProperty("操作类型")
+    private String operationType;
 
     @ApiModelProperty("操作时间")
     private LocalDateTime operationTime;
 
     @ApiModelProperty("操作耗时")
-    private Integer operationTimeConsumption;
+    private Long operationTimeConsumption;
 
-    @ApiModelProperty("操作内容")
-    private String operationContext;
+    @ApiModelProperty("操作类名")
+    private String operationClassName;
+
+    @ApiModelProperty("操作方法名")
+    private String operationMethodName;
+
+    @ApiModelProperty("操作方法参数")
+    private String operationMethodParams;
+
+    @ApiModelProperty("操作方法返回值")
+    private String operationReturnValue;
 
     @ApiModelProperty("创建人id")
     private String createUser;
@@ -55,99 +72,4 @@ public class AuditRecord implements Serializable {
     @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getOperator() {
-        return operator;
-    }
-
-    public void setOperator(String operator) {
-        this.operator = operator;
-    }
-
-    public String getOperation() {
-        return operation;
-    }
-
-    public void setOperation(String operation) {
-        this.operation = operation;
-    }
-
-    public LocalDateTime getOperationTime() {
-        return operationTime;
-    }
-
-    public void setOperationTime(LocalDateTime operationTime) {
-        this.operationTime = operationTime;
-    }
-
-    public Integer getOperationTimeConsumption() {
-        return operationTimeConsumption;
-    }
-
-    public void setOperationTimeConsumption(Integer operationTimeConsumption) {
-        this.operationTimeConsumption = operationTimeConsumption;
-    }
-
-    public String getOperationContext() {
-        return operationContext;
-    }
-
-    public void setOperationContext(String operationContext) {
-        this.operationContext = operationContext;
-    }
-
-    public String getCreateUser() {
-        return createUser;
-    }
-
-    public void setCreateUser(String createUser) {
-        this.createUser = createUser;
-    }
-
-    public LocalDateTime getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(LocalDateTime createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getUpdateUser() {
-        return updateUser;
-    }
-
-    public void setUpdateUser(String updateUser) {
-        this.updateUser = updateUser;
-    }
-
-    public LocalDateTime getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(LocalDateTime updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    @Override
-    public String toString() {
-        return "AuditRecord{" +
-                "id = " + id +
-                ", operator = " + operator +
-                ", operation = " + operation +
-                ", operationTime = " + operationTime +
-                ", operationTimeConsumption = " + operationTimeConsumption +
-                ", operationContext = " + operationContext +
-                ", createUser = " + createUser +
-                ", createTime = " + createTime +
-                ", updateUser = " + updateUser +
-                ", updateTime = " + updateTime +
-                "}";
-    }
 }

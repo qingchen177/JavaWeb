e161e8d8db8bd553b15f8d3e2c1947ea6aeb30f5
ljl <ljl@chinaliancheng.com>
2023-10-19 16:30:42 +0800

    location: company message: 自定义starter丰富：读取配置文件传入数据

--- jwt-spring-boot-autoconfigure/src/main/java/top/qingchen/TokenService.java
+++ jwt-spring-boot-autoconfigure/src/main/java/top/qingchen/TokenService.java
@@ -2,6 +2,8 @@ package top.qingchen;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Map;
+
 /**
  * @author qingchen
  * @date 19/10/2023 下午 4:10
@@ -13,6 +15,10 @@ public class TokenService {
     private TokenProperties tokenProperties;
 
     public String getToken() {
-        return JwtUtil.genJWT(tokenProperties.getClaims());
+        Map<String, Object> claims = tokenProperties.getClaims();
+        if (claims == null || claims.size() <= 0) {
+            claims.put("unknown","unknown");
+        }
+        return JwtUtil.genJWT(claims);
     }
 }

e09cb3975386e666839a737ffdce32d3584c9af2
ljl <ljl@chinaliancheng.com>
2023-12-19 13:22:06 +0800

    location: company message: kafka

---
+++ kafka/src/main/java/top/qingchen/kafka/KafkaConsumer.java
@@ -0,0 +1,17 @@
+package top.qingchen.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    //监听消费
+    @KafkaListener(topics = {"topic-test"})
+    public void onNormalMessage(ConsumerRecord<String, Object> record) {
+        System.out.println("简单消费：" + record.topic() + "-" + record.partition() + "=" +
+                record.value());
+    }
+
+}
